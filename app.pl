6addbd6eb3324b83a6af0d35e4667c30f4e765a9
Gurunandan Bhat <gbhat@pobox.com>
2017-02-11 01:50:05 +0100

    Use Dancer2 framework

---
+++ app.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+
+use Dancer2;
+use DBI;
+use File::Spec;
+use Template;
+
+use strict;
+use warnings;
+
+set 'session'      => 'Simple';
+set 'template'     => 'template_toolkit';
+set 'logger'       => 'console';
+set 'log'          => 'debug';
+set 'show_errors'  => 1;
+set 'startup_info' => 1;
+set 'warnings'     => 1;
+set 'layout'       => 'main';
+
+hook before_template_render => sub {
+
+    my $tokens = shift;
+
+    $tokens->{css_url} = request->base . 'css/';
+    $tokens->{js_url}  = request->base . 'js/';
+    $tokens->{img_url} = request->base . 'img/';
+};
+
+get '/' => sub {
+    template 'index.tt';
+};
+
+post '/upload' => sub {
+};
+
+start;
